75e6c1a207d896551a8721e553854a2417875d7a
Mohammed Sayeed <sayeedofficial6220@gmail.com>
2021-05-22 14:13:24 +0530

    Added Getting Server Side Props Funtionality

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import Head from "next/head";
-import Layout from "@/components/Layout"
-export default function Home() {
+import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+export default function Home({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
