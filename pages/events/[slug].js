59ce67ccfea7cafa93943d287699dff1a500b329
Mohammed Sayeed <sayeedofficial6220@gmail.com>
2021-05-24 21:40:32 +0530

    Changed API Request To Strapi Local Server,Formated Date and Fixed Image showing

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,14 +1,14 @@
-import { FaPencilAlt, FaTimes } from 'react-icons/fa'
-import Link from 'next/link'
-import Image from 'next/image'
-import Layout from '@/components/Layout'
-import { API_URL } from '@/config/index'
-import styles from '@/styles/Event.module.css'
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
+import Link from "next/link";
+import Image from "next/image";
+import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+import styles from "@/styles/Event.module.css";
 
 export default function EventPage({ evt }) {
   const deleteEvent = (e) => {
-    console.log('delete')
-  }
+    console.log("delete");
+  };
 
   return (
     <Layout>
@@ -19,18 +19,22 @@ export default function EventPage({ evt }) {
               <FaPencilAlt /> Edit Event
             </a>
           </Link>
-          <a href='#' className={styles.delete} onClick={deleteEvent}>
+          <a href="#" className={styles.delete} onClick={deleteEvent}>
             <FaTimes /> Delete Event
           </a>
         </div>
 
         <span>
-          {evt.date} at {evt.time}
+          {new Date(evt.date).toLocaleDateString("en-US")} at {evt.time}
         </span>
         <h1>{evt.name}</h1>
         {evt.image && (
           <div className={styles.image}>
-            <Image src={evt.image} width={960} height={600} />
+            <Image
+              src={evt.image.formats.medium.url}
+              width={960}
+              height={600}
+            />
           </div>
         )}
 
@@ -41,38 +45,38 @@ export default function EventPage({ evt }) {
         <h3>Venue: {evt.venue}</h3>
         <p>{evt.address}</p>
 
-        <Link href='/events'>
-          <a className={styles.back}>{'<'} Go Back</a>
+        <Link href="/events">
+          <a className={styles.back}>{"<"} Go Back</a>
         </Link>
       </div>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticPaths() {
-  const res = await fetch(`${API_URL}/api/events`)
-  const events = await res.json()
+  const res = await fetch(`${API_URL}/events`);
+  const events = await res.json();
 
   const paths = events.map((evt) => ({
     params: { slug: evt.slug },
-  }))
+  }));
 
   return {
     paths,
     fallback: true,
-  }
+  };
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  const res = await fetch(`${API_URL}/api/events/${slug}`)
-  const events = await res.json()
+  const res = await fetch(`${API_URL}/events?slug=${slug}`);
+  const events = await res.json();
 
   return {
     props: {
       evt: events[0],
     },
     revalidate: 1,
-  }
+  };
 }
 
 // export async function getServerSideProps({ query: { slug } }) {
